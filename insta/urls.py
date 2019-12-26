8dce3902c3267d82074d65d583276fdfccf386d8
vique254 <victoronyango051@gmail.com>
2019-12-26 08:05:07 +0300

    configure URL for accounts

--- insta/urls.py
+++ insta/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import url,include
 from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^Instagram/',include('Instagram.urls'))
+    url(r'^Instagram/',include('Instagram.urls')),
+    url(r'^accounts/',include('registration.backends.simple.urls')),
 ]
