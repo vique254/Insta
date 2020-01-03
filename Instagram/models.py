a3aae1cb457be5679c725cab21bc29299438cd4a
vique254 <victoronyango051@gmail.com>
2020-01-03 15:47:22 +0300

    create a class Post

--- Instagram/models.py
+++ Instagram/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
+    image =models.ImageField(blank=True,null=True)
+    caption = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
