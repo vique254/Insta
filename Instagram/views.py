37e341aa4c375e74e13486be0c182512a83a68f0
vique254 <victoronyango051@gmail.com>
2020-01-03 15:23:31 +0300

    make changes to files

--- Instagram/views.py
+++ Instagram/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 # Create your views here.
-@login_required(login_url='/accounts/login/')
 def welcome(request):
+   
     return render (request, 'index.html')
--- insta/urls.py
+++ insta/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from django.contrib.auth import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^Instagram/',include('Instagram.urls')),
+    url(r'^',include('Instagram.urls')),
     url(r'^accounts/',include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'}), 
 ]
