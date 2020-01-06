fb1d1c6afd8abebf49232667c9a1c2de5b21b42c
vique254 <victoronyango051@gmail.com>
2020-01-06 09:17:03 +0300

    create a sign up view function

--- Instagram/views.py
+++ Instagram/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 def welcome(request):
    
     return render (request, 'index.html')
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'auth/register_form.html', {'form': form})
