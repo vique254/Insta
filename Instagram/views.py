e593c9055dd4966fb14b44a90e87eb7bf42fd428
vique254 <victoronyango051@gmail.com>
2020-01-06 09:34:24 +0300

    import login,redirect,usercreationform,authenticate

--- Instagram/views.py
+++ Instagram/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Post
+from django.contrib.auth import login,authenticate
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 def welcome(request):
