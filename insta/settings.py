cd2a86d01c31293c1d1acdba6044a9517a08cdd0
vique254 <victoronyango051@gmail.com>
2020-01-17 01:49:35 +0300

    deploy

--- insta/settings.py
+++ insta/settings.py
@@ -131,10 +131,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
